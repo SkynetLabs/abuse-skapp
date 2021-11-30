49fe45c40897eff1063e6638f61df5564a8d4116
PJ <peterjan.brone@gmail.com>
2021-11-30 16:14:56 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    mode: 'onSubmit',
+    reValidateMode: "onChange"
+  });
+
+  const onSubmit = (data: any) => {
+    const report = {
+      reporter: {
+        name: data.reporterName,
+        email: data.reporterEmail
+      },
+      skylink: data.skylink,
+      tags: data.tags
+    }
+    console.log('reporting', JSON.stringify(report))
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(report)
+    };
+    
+    fetch('https://siasky.net/abuse', requestOptions)
+      .then(response => response.json())
+      .then(() => {
+          console.log('successfully posted data')
+      })
+      .catch(error => console.log('failed to post data, err', error));
+
+    // TODO: move this into the success handler
+    toast("Thank you for reporting!")
+    reset();
+    console.log('reset done')
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="card m-3">
+          <h5 className="card-header">Skynet Abuse Form</h5>
+          <div className="card-body row g-2">
+          <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="form-group info">
+                <label>Reporter Name</label>
+                
+                <input {...register('reporterName', { required: true })} /> 
+                {errors.reporterName && <p className="error">this field is required</p>}
+                
+              </div>
+
+              <div className="form-group info">
+                <label>Reporter Email</label>
+                <input {...register('reporterEmail', { required: true })} />
+                {errors.reporterEmail && <p className="error">this field is required</p>}
+              </div>
+
+              <div className="form-group info">
+                <label>Skylink</label>
+                <input {...register('skylink', { required: true })} />
+                {errors.skylink && <p className="error">this field is required</p>}
+              </div>
+
+              <div className="form-group">
+                <label>Tags:</label>
+
+                <div>
+                <input {...register('tags')} type="checkbox" value="copyright"/>
+                <label htmlFor="copyright">copyright violation</label>
+                </div>
+
+                <div>
+                <input {...register('tags')} type="checkbox" value="non-consensual"/>
+                <label htmlFor="non-consensual">non-consensual content</label>
+                </div>
+
+                <div>
+                <input {...register('tags')} type="checkbox" value="childabuse"/>
+                <label htmlFor="childabuse">child abuse content</label>
+                </div>
+
+                <div>
+                <input {...register('tags')} type="checkbox" value="malware"/>
+                <label htmlFor="malware">malware</label>
+                </div>
+
+                <div>
+                <input {...register('tags')} type="checkbox" value="terrorist"/>
+                <label htmlFor="terrorist">terrorist content</label>
+                </div>
+
+                <div>
+                <input {...register('tags')} type="checkbox" value="phishing"/>
+                <label htmlFor="phishing">phishing content</label>
+                </div>
+              </div>
+
+              <div>
+                <button disabled={isSubmitting} type="submit" className="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/AbuseForm.tsx
@@ -0,0 +1,34 @@
+import { Button, Card, Form } from 'react-bootstrap';
+
+// TODO
+// 
+// this is just an example on how a form should look like with bootstrap, the
+// form that is currently located in App.tsx should be refactored into the
+// structure below
+
+export function AbuseForm() {
+  return (
+    <Card style={{ width: '18rem' }}>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Card>
+  )
+}
